87004cc347093e541575498aff5ce3068f93e0a5
Hannan Ali <abdulhannanali@users.noreply.com>
2021-09-09 11:29:41 +0500

    fixed a typo in preinstall.sh

--- scripts/preinstall.sh
+++ scripts/preinstall.sh
@@ -1,6 +1,6 @@
 
 ## Preinstall Script to set the appropriate registry
 echo "Setting Github NPM Auth Token"
-yarn config set //npm.pkg.github.com/:_authToken $GITHUB_PACKAGES_TOKEN
+yarn config set //yarn.pkg.github.com/:_authToken $GITHUB_PACKAGES_TOKEN
 yarn config set @abdulhannanali:registry https://yarn.pkg.github.com/
 echo "Github NPM Token Set"
