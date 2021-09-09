100581d6e18c68295386fa784e8f8cc86e37a2d1
Hannan Ali <abdulhannanali@users.noreply.com>
2021-09-09 11:28:55 +0500

    change private registry domain to yarn

--- scripts/preinstall.sh
+++ scripts/preinstall.sh
@@ -2,5 +2,5 @@
 ## Preinstall Script to set the appropriate registry
 echo "Setting Github NPM Auth Token"
 yarn config set //npm.pkg.github.com/:_authToken $GITHUB_PACKAGES_TOKEN
-yarn config set @abdulhannanali:registry https://npm.pkg.github.com/
+yarn config set @abdulhannanali:registry https://yarn.pkg.github.com/
 echo "Github NPM Token Set"
