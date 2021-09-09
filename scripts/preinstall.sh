7ec0ff120c91ee474004291b8e089fd71e507493
Hannan Ali <abdulhannanali@users.noreply.com>
2021-09-09 11:38:55 +0500

    add curly braces to token in preinstall.sh

--- scripts/preinstall.sh
+++ scripts/preinstall.sh
@@ -1,7 +1,7 @@
 
 ## Preinstall Script to set the appropriate registry
 echo "Setting Github NPM Auth Token"
-yarn config set https://npm.pkg.github.com/:_authToken $GITHUB_PACKAGES_TOKEN
+yarn config set https://npm.pkg.github.com/:_authToken ${GITHUB_PACKAGES_TOKEN}
 yarn config set @abdulhannanali:registry https://npm.pkg.github.com/
 yarn config set @abdulhannanali https://npm.pkg.github.com
 echo "Github NPM Token Set"
